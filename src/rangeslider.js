bca56869b8632eba252f93ee461504952ebe8b2d
Mike Pennisi <mike@mikepennisi.com>
2014-03-11 19:16:35 -0400

    Add support for floating point boundaries
    According to the W3C's documentation of the `range` input type [1], the
    `min`, `max`, `step`, and `value` attributes may all be specified as
    floating point numbers.
    
    [1] http://www.w3.org/TR/html-markup/input.range.html

--- src/rangeslider.js
+++ src/rangeslider.js
@@ -108,10 +108,10 @@
         }
 
         this.identifier = 'js-' + pluginName + '-' + +new Date();
-        this.value      = parseInt(this.$element[0].value) || 0;
-        this.min        = parseInt(this.$element[0].getAttribute('min')) || 0;
-        this.max        = parseInt(this.$element[0].getAttribute('max')) || 0;
-        this.step       = parseInt(this.$element[0].getAttribute('step')) || 1;
+        this.value      = parseFloat(this.$element[0].value) || 0;
+        this.min        = parseFloat(this.$element[0].getAttribute('min')) || 0;
+        this.max        = parseFloat(this.$element[0].getAttribute('max')) || 0;
+        this.step       = parseFloat(this.$element[0].getAttribute('step')) || 1;
         this.$range     = $('<div class="' + this.options.rangeClass + '" />');
         this.$fill      = $('<div class="' + this.options.fillClass + '" />');
         this.$handle    = $('<div class="' + this.options.handleClass + '" />');
@@ -205,7 +205,7 @@
         value = this.getValueFromPosition(left);
 
         // Snap steps
-        if (this.step > 1) {
+        if (this.step !== 1) {
             value = Math.ceil((value) / this.step ) * this.step;
             left = this.getPositionFromValue(value);
         }
@@ -249,7 +249,7 @@
     Plugin.prototype.getValueFromPosition = function(pos) {
         var percentage, value;
         percentage = ((pos) / (this.maxHandleX)) * 100;
-        value = Math.ceil(((percentage/100) * (this.max - this.min)) + this.min);
+        value = this.step * Math.ceil((((percentage/100) * (this.max - this.min)) + this.min) / this.step);
 
         return value;
     };
