0d0cf357aac34600ff57ff3b48789481f09d4b16
André Ruffert <andre@andreruffert.com>
2015-04-16 19:04:33 +0200

    release: v1.2.0

--- dist/rangeslider.js
+++ dist/rangeslider.js
@@ -1,4 +1,4 @@
-/*! rangeslider.js - v1.1.0 | (c) 2015 @andreruffert | MIT license | https://github.com/andreruffert/rangeslider.js */
+/*! rangeslider.js - v1.2.0 | (c) 2015 @andreruffert | MIT license | https://github.com/andreruffert/rangeslider.js */
 (function(factory) {
     'use strict';
 
