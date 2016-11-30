andreruffert/rangeslider.js

commit 53fa2d1b94ec5b99185cd4cdbe133650a1d41146

migrate(jQuery 3.x): use e.pageX/pageY instead of e.clientX/clientY
jQuery 3.x removed the clientX/clientY calculations which caused some
issues in iOS 9.

https://jquery.com/upgrade-guide/3.0/#breaking-change-event-pagex-and-ev
ent-pagey-normalization-removed

close #224

dist/rangeslider.js
@@ -403,13 +403,14 @@
             pageCoordinate = 0;
 
         if (typeof e['page' + ucCoordinate] !== 'undefined') {
-            pageCoordinate = e['client' + ucCoordinate];
+            pageCoordinate = e['page' + ucCoordinate];
         }
+        // IE8 support :)
         else if (typeof e.originalEvent['client' + ucCoordinate] !== 'undefined') {
             pageCoordinate = e.originalEvent['client' + ucCoordinate];
         }
-        else if (e.originalEvent.touches && e.originalEvent.touches[0] && typeof e.originalEvent.touches[0]['client' + ucCoordinate] !== 'undefined') {
-            pageCoordinate = e.originalEvent.touches[0]['client' + ucCoordinate];
+        else if (e.originalEvent.touches && e.originalEvent.touches[0] && typeof e.originalEvent.touches[0]['page' + ucCoordinate] !== 'undefined') {
+            pageCoordinate = e.originalEvent.touches[0]['page' + ucCoordinate];
         }
         else if(e.currentPoint && typeof e.currentPoint[this.COORDINATE] !== 'undefined') {
             pageCoordinate = e.currentPoint[this.COORDINATE];

src/rangeslider.js
@@ -402,13 +402,14 @@
             pageCoordinate = 0;
 
         if (typeof e['page' + ucCoordinate] !== 'undefined') {
-            pageCoordinate = e['client' + ucCoordinate];
+            pageCoordinate = e['page' + ucCoordinate];
         }
+        // IE8 support :)
         else if (typeof e.originalEvent['client' + ucCoordinate] !== 'undefined') {
             pageCoordinate = e.originalEvent['client' + ucCoordinate];
         }
-        else if (e.originalEvent.touches && e.originalEvent.touches[0] && typeof e.originalEvent.touches[0]['client' + ucCoordinate] !== 'undefined') {
-            pageCoordinate = e.originalEvent.touches[0]['client' + ucCoordinate];
+        else if (e.originalEvent.touches && e.originalEvent.touches[0] && typeof e.originalEvent.touches[0]['page' + ucCoordinate] !== 'undefined') {
+            pageCoordinate = e.originalEvent.touches[0]['page' + ucCoordinate];
         }
         else if(e.currentPoint && typeof e.currentPoint[this.COORDINATE] !== 'undefined') {
             pageCoordinate = e.currentPoint[this.COORDINATE];

commit 5d295c562d1c560a03ff20c520d07706404e082a

fix: getRelativePosition use `clientX/clientY` instead of `pageX/pageY`
close #236
close #238
close #239

Bug introduced in 53fa2d1b94ec5b99185cd4cdbe133650a1d41146

dist/rangeslider.js
@@ -402,15 +402,16 @@
             rangePos = this.$range[0].getBoundingClientRect()[this.DIRECTION],
             pageCoordinate = 0;
 
-        if (typeof e['page' + ucCoordinate] !== 'undefined') {
-            pageCoordinate = e['page' + ucCoordinate];
-        }
-        // IE8 support :)
-        else if (typeof e.originalEvent['client' + ucCoordinate] !== 'undefined') {
+        if (typeof e.originalEvent['client' + ucCoordinate] !== 'undefined') {
+            console.log('client' + ucCoordinate);
             pageCoordinate = e.originalEvent['client' + ucCoordinate];
         }
-        else if (e.originalEvent.touches && e.originalEvent.touches[0] && typeof e.originalEvent.touches[0]['page' + ucCoordinate] !== 'undefined') {
-            pageCoordinate = e.originalEvent.touches[0]['page' + ucCoordinate];
+        else if (
+          e.originalEvent.touches &&
+          e.originalEvent.touches[0] &&
+          typeof e.originalEvent.touches[0]['client' + ucCoordinate] !== 'undefined'
+        ) {
+            pageCoordinate = e.originalEvent.touches[0]['client' + ucCoordinate];
         }
         else if(e.currentPoint && typeof e.currentPoint[this.COORDINATE] !== 'undefined') {
             pageCoordinate = e.currentPoint[this.COORDINATE];

src/rangeslider.js
@@ -401,15 +401,16 @@
             rangePos = this.$range[0].getBoundingClientRect()[this.DIRECTION],
             pageCoordinate = 0;
 
-        if (typeof e['page' + ucCoordinate] !== 'undefined') {
-            pageCoordinate = e['page' + ucCoordinate];
-        }
-        // IE8 support :)
-        else if (typeof e.originalEvent['client' + ucCoordinate] !== 'undefined') {
+        if (typeof e.originalEvent['client' + ucCoordinate] !== 'undefined') {
+            console.log('client' + ucCoordinate);
             pageCoordinate = e.originalEvent['client' + ucCoordinate];
         }
-        else if (e.originalEvent.touches && e.originalEvent.touches[0] && typeof e.originalEvent.touches[0]['page' + ucCoordinate] !== 'undefined') {
-            pageCoordinate = e.originalEvent.touches[0]['page' + ucCoordinate];
+        else if (
+          e.originalEvent.touches &&
+          e.originalEvent.touches[0] &&
+          typeof e.originalEvent.touches[0]['client' + ucCoordinate] !== 'undefined'
+        ) {
+            pageCoordinate = e.originalEvent.touches[0]['client' + ucCoordinate];
         }
         else if(e.currentPoint && typeof e.currentPoint[this.COORDINATE] !== 'undefined') {
             pageCoordinate = e.currentPoint[this.COORDINATE];